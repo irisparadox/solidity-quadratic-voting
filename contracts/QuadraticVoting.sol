52019be568f143c677d44065e73afff229c8daf2
irisparadox <saavedra.gabriel083@gmail.com>
2026-05-05 10:14:18 +0200

    modifiers and functions (I forgor to add)

--- contracts/QuadraticVoting.sol
+++ contracts/QuadraticVoting.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract QuadraticVoting {
     enum VotingState { CLOSED, OPEN }
 
     struct Participant {
         bool registered;
+        bool active;
         uint tokensOwned;
     }
 
@@ -33,9 +36,86 @@ contract QuadraticVoting {
     mapping(address => uint) public pendingTokenRetrieval;
     mapping(address => uint) public pendingEtherRetrieval;
 
-    VotingState public state;
+    ERC20 public token;
+    uint public tokenPrice;
+    uint public maxTokens;
 
-    constructor() {
+    VotingState public state;
+    address public owner;
+
+    constructor(uint _tokenPrice, uint _maxTokens) {
+        owner = msg.sender;
         state = VotingState.CLOSED;
+
+        tokenPrice = _tokenPrice;
+        maxTokens = _maxTokens;
+
+        // token = new ERC20();
     }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier inState(VotingState s) {
+        require(state == s, "Invalid state");
+        _;
+    }
+
+    modifier onlyParticipant() {
+        require(participants[msg.sender].registered, "Not a participant");
+        _;
+    }
+
+    modifier onlyActiveParticipant() {
+        require(
+            participants[msg.sender].registered &&
+            participants[msg.sender].active,
+            "Not active participant"
+        );
+        _;
+    }
+
+    function openVoting() external payable onlyOwner inState(VotingState.CLOSED) {
+        totalBudget = msg.value;
+        state = VotingState.OPEN;
+    }
+
+    function addParticipant() external payable {
+        require(!participants[msg.sender].registered, "Participant is already registered");
+        require(msg.value >= tokenPrice, "Not enough Ether");
+
+        uint tokensToMint = msg.value / tokenPrice;
+
+        participants[msg.sender] = Participant({
+            registered: true,
+            active: true,
+            tokensOwned: tokensToMint
+        });
+
+        ++numParticipants;
+        //token.mint(msg.sender, tokensToMint);
+    }
+
+    function removeParticipant() external onlyParticipant() {
+        Participant storage p = participants[msg.sender];
+        require(p.active, "Participant was already removed");
+        p.active = false;
+    }
+
+    function addProposal(string memory title, uint budget, address executable)
+        external
+        onlyParticipant()
+        inState(VotingState.OPEN) {
+            proposals[nextProposalId++] = Proposal({
+                title: title,
+                budget: budget,
+                votes: 0,
+                approved: false,
+                canceled: false,
+                isSignaling: budget == 0,
+                executable: executable
+            });
+        }
 }
